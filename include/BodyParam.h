1625ba1eaadb336156ba534817e00370a76959c6
Nathaniel <nagoldfarb@wpi.edu>
2022-04-23 08:29:34 -0400

    added files for reading the AMBF files

---
+++ include/BodyParam.h
@@ -0,0 +1,34 @@
+#ifndef BODYPARAM_H
+#define BODYPARAM_H
+#include <iostream>
+#include <Utilities.h>
+
+/*
+ * BodyParam Class is used to hold values of Body parameter
+ */
+using namespace RBDLModel;
+
+class BodyParam
+{
+public:
+    BodyParam(YAML::Node bodyNode);
+    ~BodyParam(void);
+    inline double Mass() { return mass_; }
+    inline Vector3d InertialOffsetPosition() { return inertial_offset_position_; }
+    inline Vector3d InertialOffsetOrientation() { return inertial_offset_orientation_; }
+
+    inline Math::Matrix3d Inertia() { return inertia_; }
+
+private:
+
+    std::string name_;
+    double mass_{0.00000001};
+    Math::Matrix3d inertia_;
+    Vector3d inertial_offset_position_;
+    //substitute this value to com in RBDL
+    Vector3d inertial_offset_orientation_;
+    std::string trimTrailingSpaces(YAML::Node bodyNode);
+
+};
+
+#endif // AFRIGIDBODY_H
---
+++ src/RBDLModelErrors.cpp
@@ -0,0 +1,19 @@
+#include "RBDLModelErrors.h"
+
+namespace RBDLModel
+{
+namespace ModelErrors
+{
+
+RBDLModelError::RBDLModelError(std::string text): text(text) {}
+
+const char* RBDLModelError::what() const noexcept
+{
+  return text.c_str();
+}
+
+RBDLModelInvalidFilePathError::RBDLModelInvalidFilePathError(std::string text) :RBDLModelError(text) {}
+RBDLModelMissingParameterError::RBDLModelMissingParameterError(std::string text) :RBDLModelError(text) {}
+
+}
+}
